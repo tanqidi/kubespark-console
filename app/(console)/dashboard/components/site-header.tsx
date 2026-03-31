622eb45d8f70fc8666a4c9e9dc5b2fc8cf9fe0ba
tanqidi <1729746565@qq.com>
2026-03-31 16:34:19 +0800

    update

--- app/(console)/dashboard/components/site-header.tsx
+++ app/(console)/dashboard/components/site-header.tsx
@@ -15,7 +15,7 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">Documents</h1>*/}
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+          {/*<Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
               href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(console)/dashboard"
               rel="noopener noreferrer"
@@ -24,7 +24,7 @@ export function SiteHeader() {
             >
               GitHub
             </a>
-          </Button>
+          </Button>*/}
           <ThemeSelector />
           <ModeToggle />
         </div>
